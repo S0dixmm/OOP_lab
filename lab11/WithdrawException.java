95b59988e19a2f2edb35e74508b137799499d27e
S0dixmm <143612070+S0dixmm@users.noreply.github.com>
2025-03-07 08:04:46 +0700

    Create WithdrawException.java

---
+++ lab11/WithdrawException.java
@@ -0,0 +1,16 @@
+package week11;
+
+/**
+ *
+ * @author User
+ */
+public class WithdrawException extends Exception {
+
+    public WithdrawException() {
+        super();
+    }
+
+    public WithdrawException(String s) {
+        super(s);
+    }
+}
